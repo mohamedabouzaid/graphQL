7559c9f231d18f552024e757f1965a6fa401766f
abouzaid <mohmmdabouzaid@gmail.com>
2022-02-16 21:09:12 +0200

    add schema for comment

--- server.js
+++ server.js
@@ -45,19 +45,28 @@ const schema = buildSchema(`
     lastName: String!
     age: Int
   }
+  type Comment{
+    commentContent: String
+}
   type Post{
     content: String!
     user: User!
+    comments: [Comment]!
   }
+  
   type Query{
     hello: String
     getMyPosts(token: String): [Post!]!
     getAllPosts: [Post!]!
+    getComments(token: String, postID: String): [Comment]!
   }
   type Mutation{
     createUser(userData: UserRegistrationInput): User
     loginUser(username: String, password: String): LoginPayload
     postCreate(token:String, content:String): String
+    postEdit(token: String, postID: String, newContent: String): String
+    postDelete(token: String, postID: String): String
+    createComment(token: String, postID: String, commentContent: String): String
   }
 `);
 
